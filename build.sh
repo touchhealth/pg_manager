ad609d0f19013bd04bc14021617c1840089e12ac
dgiorgio <douglas.giorgio@gmail.com>
2021-12-09 17:57:53 -0300

    update docker repository

--- build.sh
+++ build.sh
@@ -10,15 +10,15 @@ echo "
 ################################################################################
 ################### Build pgman ##############################################
 ################################################################################
-Image: ${REGISTRY}dgiorgio/pgman:${pgman_version}"
-docker build -t "${REGISTRY}dgiorgio/pgman:${pgman_version}" -t "${REGISTRY}dgiorgio/pgman:latest" .
+Image: ${REGISTRY}touchhealth/pgman:${pgman_version}"
+docker build -t "${REGISTRY}touchhealth/pgman:${pgman_version}" -t "${REGISTRY}touchhealth/pgman:latest" .
 
 echo "All images successfully built."
 
 # push
 if [ "${1}" == "push" ]; then
-  docker push "${REGISTRY}dgiorgio/pgman:${pgman_version}"
-  docker push "${REGISTRY}dgiorgio/pgman:latest"
+  docker push "${REGISTRY}touchhealth/pgman:${pgman_version}"
+  docker push "${REGISTRY}touchhealth/pgman:latest"
 
   echo "All images sent successfully."
 fi
